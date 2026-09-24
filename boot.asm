012b2d83e1db0d8fbf8d229efbe846b336859216
rituparnabagdandapat-commits <rituparna.bagdandapat@gmail.com>
2026-09-24 13:58:10 +0530

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,19 +1,7 @@
-bits 32
+[BITS 64]
 section.text
-    align 4
-    dd 0x1BADB002
-    dd 0x00
-    dd - (0x1BADB002 + 0x00)
-
-global start
-extern kmain
-
-start:
+global _start
+_start:
     cli
-    mov esp, stack_space
-    call kmain
     hlt
-
-section.bss
-resb 8192
-stack_space:
+    jmp _start
