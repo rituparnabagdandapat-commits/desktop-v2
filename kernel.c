c18f64e498956e6e2d2558f20a7400f0fda3f80e
rituparnabagdandapat-commits <rituparna.bagdandapat@gmail.com>
2026-09-26 11:22:22 +0530

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,12 +1,12 @@
 #include "limine.h"
 #include <stdint.h>
-static volatile struct limine_framebuffer_request fb = {.id=LIMINE_FRAMEBUFFER_REQUEST,.revision=0};
+static volatile struct limine_framebuffer_request fb_req = {.id=LIMINE_FRAMEBUFFER_REQUEST,.revision=0};
 void _start(void){
- for(;;){
-  if(fb.response && fb.response->framebuffer_count>0){
-   struct limine_framebuffer *f=fb.response->framebuffers[0];
-   for(uint64_t y=0;y<f->height;y++) for(uint64_t x=0;x<f->width;x++) ((uint32_t*)f->address)[y*(f->pitch/4)+x]=0x0078D7;
-  }
-  __asm__("hlt");
- }
+    if(fb_req.response==0||fb_req.response->framebuffer_count<1) for(;;) __asm__("hlt");
+    struct limine_framebuffer *fb=fb_req.response->framebuffers[0];
+    for(uint64_t y=0;y<fb->height;y++){
+        uint8_t *line=(uint8_t*)fb->address + y*fb->pitch;
+        for(uint64_t x=0;x<fb->width;x++){ line[x*4+0]=0x8A; line[x*4+1]=0x3A; line[x*4+2]=0x1E; }
+    }
+    for(;;) __asm__("hlt");
 }
